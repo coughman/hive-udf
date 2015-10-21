262938b1cdd671ef3bcf548bb21852ef50ca9b85
Kaufman Ng <kaufman@cloudera.com>
2015-10-21 12:23:35 -0400

    now uses Apache Common DigestUtils for generating MD5.  Added unit test

--- src/test/java/com/cloudera/hive/udf/examples/MessageDigestUDFTest.java
+++ src/test/java/com/cloudera/hive/udf/examples/MessageDigestUDFTest.java
@@ -1,11 +1,23 @@
 package com.cloudera.hive.udf.examples;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class MessageDigestUDFTest {
+	MessageDigestUDF udf = new MessageDigestUDF();
 
 	
+	@Test
+	public void test() {
+		String hash = udf.evaluate("kaufman");
+		assertNotNull(hash);
+		assertEquals("ec202a34f44a632332c71e3700466f7b", hash);
+		
+		hash = udf.evaluate("cloudera");
+		assertNotNull(hash);
+		assertEquals("06915a50d28717172b154d86a3399019", hash);
+	}
 
 }
